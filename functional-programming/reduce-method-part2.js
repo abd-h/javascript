023cec43d5d3a7c27a095c1664d87d8e29021089
ABDALLA HUSSEIN <A.HUSSEIN1934@gmail.com>
2022-02-06 23:57:24 +0000

    Added more practice on the reduce method.

--- functional-programming/reduce-method-part2.js
+++ functional-programming/reduce-method-part2.js
@@ -84,10 +84,143 @@ const fruitBasket = [
     'orange', 'apple', 'banana', 'cherry', 'orange', 'fig'
 ];
 
-const count = fruitBasket.reduce((tally, fruit) => {
+const count =fruitBasket.reduce((tally, fruit) => {
     tally[fruit] = (tally[fruit] || 0) + 1;
-    return tally
+    return tally;
+}, {})
+console.log(count); /* returns {banana: 2, cherry: 3, orange: 3, apple: 2, fig: 1}
 
+To tally items in an array out initial value must be an empty object, not an empty array 
+like it was in  the last example.
+
+Since we are going to be returning an object we can now store key-value pairs in the total.*/
+
+const total1 = fruitBasket.reduce((tally, fruit) => {
+    tally[fruit] = 1;
+    return tally;
 }, {});
-console.log(count); // returns 
+console.log(total1); /* returns {banana: 1, cherry: 1, orange: 1, apple: 1, fig: 1}
 
+On our first pass, we want the name of the first key to be our current value and we want to give it a
+value of 1.
+
+This gives us an object with all the fruit as keys, each with a value of 1. We want the amount of each 
+fruit to increase if they repeat.
+
+To do this, on our second loop we check if our total contain a key with the current fruit of the reducer. If it does't then we creat it. If it does then we increment the amount by one.*/
+
+const tallyFruit = fruitBasket.reduce((tally, fruit) => {
+    (!tally[fruit])? tally[fruit] = 1
+    : tally[fruit] = (tally[fruit]) + 1;
+    return tally;
+}, {});
+console.log(tallyFruit);/* In line 87 is the exact same logic in a more concise way up top.
+
+            Flattening an array of arrays with the Reduce Method In JavaScript
+
+We can use reduce to flatten nested amounts into a single array.
+
+We se the initial value to an empty array and then concatenate the current value to the total.*/
+
+const data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+const flat = data.reduce((total, amount) => {
+    return total.concat(amount);
+}, []);
+console.log(flat);/* returns [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+More often than not, information is nested in more complicated ways. For instance, 
+lets say we just want all the colors in the data variable below.*/
+
+const dataOfColors = [
+    {a: "happy", b: "robin", c: ["green",  "blue"]},
+    {a: "tired", b: "panther", c: ["green", "black", "orange", "blue"]}, 
+    {a: "sad", b: "goldfish", c: ["green", "red"]}
+];
+
+/* We're going to step through each object and pull out the colours. We do this by pointing 
+amount.c for each object in the array. We then use a forEach loop to push every value in the nested array into out total */
+
+const colors = dataOfColors.reduce((total, amount) => {
+    amount.c.forEach(a => total.push(a));
+    return total;
+}, []);
+console.log(colors); /* ['green', 'blue', 'green', 'black', 'orange', 'blue', 'green', 'red']
+
+If we only need unique number then we can check to see of the number already exist before we push 
+it.*/
+
+const uniqueColors = dataOfColors.reduce((total, amount) => {
+    amount.c.forEach(a => {
+        console.log(total);
+       if(total.indexOf(a) === -1){
+           total.push(a)
+       }
+    });
+    return total;
+}, []);
+
+console.log(uniqueColors); /* returns ) ['green', 'blue', 'black', 'orange', 'red']
+
+Piping with reduce
+An interesting aspect of the reduce method in JavaScript is that you can reduce  over functions as well
+as numbers and strings. 
+
+Let's say we have a collection of simple mathematical functions. These functioins allow us to increment,
+decrement, double and halve an amount. */
+
+function increment(input) {
+    return input + 1;
+}
+
+function decrement(input) {
+    return input - 1;
+}
+
+function double(input) {
+    return input * 2;
+}
+
+function halve(input) {
+    return input / 2;
+}
+
+/* For whatever reason, we need to increment, then double, then decrement an amount.
+
+You could write a function that takes an input, and returns (input +1) * 2 - 1.
+The problem is that we know we are going to need to increment the amount three times, then double it, then decrement it, and then halve it at some point in the future. We don't want to have to have to reweite our function wvery time so we going to use reduce ot create a pipeline
+
+A pipeline is a term used for a list of function that transform some initial value into a funal value.
+Our pipeline will consist of our three functions in the order that we want to use them. */
+
+let pipeline = [increment, double, decrement ];
+
+/* Instead of reducing an array of values we reduce over our pipeline of functioins. This works 
+because we set the initial value as the amount we want to transform. */
+
+const result = pipeline.reduce((total, func) => {
+    return func(total);
+}, 1);
+
+console.log(result); /* returns 3
+
+Because the pipeline is an array, it can be easily modified. If we want to decrement somthing three times, then double it, decrement it, and halve it then we just alter the pipeline.*/;
+
+let pipeline2 = [
+    increment,
+    increment, 
+    double, 
+    decrement,
+    halve
+];
+/*
+The reduce function stays exactly the same.
+
+Silly Mistakes to avoid
+
+If you don't pass in an initial value, reduce will assume the first item in your array is your initial value. This worked fine in the first few examples because we were adding up a list of numbers.
+
+If you're trying to tally upfruit, and you leave out the initial value then things get weird. Not entering an initial value is an easy mistake to make and one of the first things you should check when debugging. 
+
+Another common mistake is to forget to return te total. You must return somthing for the reduce function to work. Always double check and make sure that you're actually returning the value you want.
+*/
