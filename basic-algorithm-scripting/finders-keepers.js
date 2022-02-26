22271543b40b1d094681a0b0cf25c636aa831445
ABDALLA HUSSEIN <A.HUSSEIN1934@gmail.com>
2022-02-26 21:45:09 +0000

    completed Finder Keepers challenge

---
+++ basic-algorithm-scripting/finders-keepers.js
@@ -0,0 +1,19 @@
+/*  Finders Keepers
+Create a function that looks throughh an array arr and returns the first element
+in it that passes a 'truth test'. This means that given an element x, the 'thuth test' is passed if func(x) is true. If no element passes the test, returns */
+
+function findElement(arr, func) {
+    let num = arr.find(a => {
+        if(func(a)) {
+            return a;
+        } else {
+            return undefined
+        }
+    });
+    return num
+}
+console.log(findElement([1, 2, 3, 4], num => num % 2 === 0));
+
+console.log(findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; })) // return 8.
+
+console.log(findElement([1, 3, 5, 9], function(num) { return num % 2 === 0; })) // return undefined.
