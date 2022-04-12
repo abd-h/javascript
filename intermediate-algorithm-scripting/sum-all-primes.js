cec03e1fa7e2bdba4dc2cd37a71a99ddbfaa35a0
ABDALLA HUSSEIN <A.HUSSEIN1934@gmail.com>
2022-04-13 00:06:07 +0100

    completed sum of all primes challenge;

---
+++ intermediate-algorithm-scripting/sum-all-primes.js
@@ -0,0 +1,53 @@
+/*  Sum All Primes
+    A prime number is a whole number greater than 1 with exactly two divisors: 1 and itself. 
+    For example, 2 is aprime number because it is only divisible by 1 and 2. In contrast, 4 is not prime since it is divisible by 1, 2 and 4.
+    
+    Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.
+    
+    The Plan
+    1. Decalre a function that checks for prime Numbers
+    2. Create a for loop
+    */
+
+    // function isPrime(num) {
+    //     for(let i = 2; i < num; i++) {
+    //         if(num % i === 0) {
+    //             return false;
+    //         } 
+    //     }
+    //     return true;
+    // }
+    // console.log(isPrime(9))
+    
+
+    // function sumPrimes(num) {
+    //     let s = 2;
+    //     for(let i = 3; i <= num; i+= 2){
+    //        if(isPrime(i)) {
+    //         s += i;
+    //        } 
+    //     }
+    //     return s;
+    // }
+    // console.log(sumPrimes(977));
+
+    function isPrime(num) {
+        for(let i = 2; i < num; i++) {
+            if(num % i === 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function sumPrimes(num) {
+        let total = 2;
+        for(let i = 3; i <= num; i += 2) {
+            if(isPrime(i)) {
+                total += i;
+            }
+        }
+        return total; 
+    }
+    console.log(sumPrimes(10))
+
